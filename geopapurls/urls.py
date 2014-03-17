5687d9b726201087364ef83f903622e16a89b318
giohappy <giohappy@gmail.com>
2014-03-17 21:49:47 +0100

    Fix login requirements

--- geopapurls/urls.py
+++ geopapurls/urls.py
@@ -9,8 +9,8 @@ urlpatterns = patterns('',
     url(r'^$', 'geopapurls.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^layers/$',login_required(LayersView.as_view()),name='layers-list'),
-    url(r'^mapurls/$',login_required(MapurlsView.as_view())),
-    url(r'^mapurls/(?P<pk>\d+)/(?P<action>[a-zA-Z]+)?$',login_required(MapurlDetailView.as_view())),
+    url(r'^mapurls/$',MapurlsView.as_view()),
+    url(r'^mapurls/(?P<pk>\d+)/(?P<action>[a-zA-Z]+)?$',MapurlDetailView.as_view()),
     url(r'^login/$', 'django.contrib.auth.views.login',name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout',{'next_page': settings.LOGOUT_NEXT_PAGE},name='logout')
 )
