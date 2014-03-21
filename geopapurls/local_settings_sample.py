d77e78dcac93263f7c8dde8571881b31765e38df
giohappy <giohappy@gmail.com>
2014-03-21 16:05:38 +0100

    Logging moved to local settings

--- geopapurls/local_settings_sample.py
+++ geopapurls/local_settings_sample.py
@@ -8,3 +8,32 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'info@yourdomain.com'
 EMAIL_HOST_PASSWORD = 'password'
 EMAIL_USE_TLS = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'geopapurls': {
+            'handlers': ['suggested_services_email'],
+            'level': 'INFO',
+            'propagate': True,
+        }
+    }
+}
--- geopapurls/settings.py
+++ geopapurls/settings.py
@@ -87,43 +87,7 @@ EMAIL_PORT = 25
 EMAIL_HOST_USER = None
 EMAIL_HOST_PASSWORD =None
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        },
-        'suggested_services': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': '/home/giova/Desktop/suggestions_arrived.log',
-        },
-         'suggested_services_email': {
-            'level': 'INFO',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-        'geopapurls': {
-            'handlers': ['suggested_services_email'],
-            'level': 'INFO',
-            'propagate': True,
-        }
-    }
-}
+LOGGING = {}
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ['DATABASE_URL'])
