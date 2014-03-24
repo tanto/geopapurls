6023e4d76373d28724ffa2cd6b921477a5b7333e
root <root@muttley.spaziogis.it>
2014-03-24 10:55:17 +0100

    wsgi settings setup

--- geopapurls/wsgi.py
+++ geopapurls/wsgi.py
@@ -14,19 +14,21 @@ framework.
 
 """
 import os
+import sys
+import glob
 
-# We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
-# if running multiple sites in the same mod_wsgi process. To fix this, use
-# mod_wsgi daemon mode with each site in its own daemon process, or use
-# os.environ["DJANGO_SETTINGS_MODULE"] = "geopapurls.settings"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geopapurls.settings")
+if 'test' in sys.argv:
+    env_dir = os.path.join('tests', 'config')
+else:
+    env_dir = 'config'
+env_vars = glob.glob(os.path.join(env_dir, '*'))
+for env_var in env_vars:
+    with open(env_var, 'r') as env_var_file:
+        key = env_var.split(os.sep)[-1]
+        val = env_var_file.read().strip()
+        os.environ.setdefault(key,val)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'geopapurls.settings'
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
